dcba5d9904772591094f06f0ebeb26ae86bdac7e
khee2 <21keunghee@gmail.com>
2024-12-23 23:57:59 +0900

    Feat: Category 초기 세팅

---
+++ src/main/java/com/pathfinder/spot/domain/category/Category.java
@@ -0,0 +1,40 @@
+package com.pathfinder.spot.domain.category;
+
+import com.pathfinder.spot.domain.placefav.PlaceFav;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = PROTECTED)
+@SQLDelete(sql = "UPDATE category SET status = 'DELETED' where id = ?")
+@SQLRestriction("status = 'ACTIVE'")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory")
+    private List<Category> subCategories = new ArrayList<>();
+
+    @Column(nullable = false, length = 100)
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category")
+    private List<PlaceFav> placeFavs;
+}
