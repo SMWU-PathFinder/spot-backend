06d61ed1a6f6e80a73d152b151348fb3c108c3fe
khee2 <21keunghee@gmail.com>
2024-12-26 22:37:52 +0900

    [#3] Fix: 부모 카테고리 삭제 및 BaseEntity 추가

--- src/main/java/com/pathfinder/spot/domain/category/Category.java
+++ src/main/java/com/pathfinder/spot/domain/category/Category.java
@@ -1,5 +1,6 @@
 package com.pathfinder.spot.domain.category;
 
+import com.pathfinder.spot.common.entity.BaseEntity;
 import com.pathfinder.spot.domain.placefav.PlaceFav;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -20,17 +21,17 @@ import static lombok.AccessLevel.PROTECTED;
 @NoArgsConstructor(access = PROTECTED)
 @SQLDelete(sql = "UPDATE category SET status = 'DELETED' where id = ?")
 @SQLRestriction("status = 'ACTIVE'")
-public class Category {
+public class Category extends BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_category_id")
-    private Category parentCategory;
-
-    @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Category> subCategories = new ArrayList<>();
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "parent_category_id")
+//    private Category parentCategory;
+//
+//    @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Category> subCategories = new ArrayList<>();
 
     @Column(nullable = false, length = 100)
     private String categoryName;
